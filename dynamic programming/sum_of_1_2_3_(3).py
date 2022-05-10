39dc690652d597d3bac21c6867f7b790e8ff5b7a
DESKTOP-B2F7HQT\user <chlghksdyd31@gmail.com>
2022-05-10 11:47:59 +0900

    baekjoon problem number 15988

---
+++ dynamic programming/sum_of_1_2_3_(3).py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0]*1000001
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4,1000001):
+    dp[i] = dp[i-1]%1000000009+dp[i-2]%1000000009+dp[i-3]%1000000009
+
+for i in range(n):
+    print(dp[int(input())]%1000000009)
