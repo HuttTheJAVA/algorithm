d8b5babbc7963b15c8140f67fc06fd5c9b8232c0
DESKTOP-B2F7HQT\user <chlghksdyd31@gmail.com>
2022-05-10 11:57:22 +0900

    baekjoon problem number 18353

---
+++ dynamic programming/arrange_soldiers.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+n_lst = list(map(int,input().split()))
+
+dp = [1]*(2001)
+
+for i in range(n):
+    for j in range(i):
+        if n_lst[j]>n_lst[i]:
+            dp[i] = max(dp[i],dp[j]+1)
+
+print(n-max(dp))
